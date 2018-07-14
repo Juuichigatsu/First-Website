a84e712fc92289c8278180bda4aa9fa8a28aab95
Juuichigatsu <j.d.hennig@hotmail.com>
2018-07-14 15:31:20 +0200

    new project

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var sourcemaps = require("gulp-sourcemaps");
+var autoprefixer = require('gulp-autoprefixer');
+gulp.task("sass", function () {
+	return gulp.src("./scss/**/*.scss").pipe(sourcemaps.init()).pipe(sass().on("error", sass.logError)).pipe(sourcemaps.write()).pipe(autoprefixer({
+		browsers: ['last 2 versions']
+		, cascade: false
+	})).pipe(gulp.dest("./css"))
+});
+gulp.task('watch', function () {
+	gulp.watch('scss/*.scss', ['sass']);
+});
